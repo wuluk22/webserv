7f3ea647d4993fbaae04002c8ebb06211c633cae
noureh10 <noureh.it@outlook.com>
2024-10-08 16:52:14 +0200

    Adding methods signature for ConfigParser

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -1,2 +1,46 @@
 #include "ConfigParser.hpp"
 
+ConfigParser::ConfigParser(std::string config_file_path) {
+
+}
+
+ConfigParser::~ConfigParser(void) {
+
+}
+ConfigParser::ConfigParser(const ConfigParser &copy) {
+
+}
+
+ConfigParser& ConfigParser::operator=(const ConfigParser &assign) {
+
+}
+
+
+std::string ConfigParser::getRootDirectory(void) const {
+
+}
+
+std::string ConfigParser::getLogFile(void) const {
+
+}
+
+std::map<std::string, std::string> ConfigParser::getVirtualHosts(void) const {
+
+}
+
+std::map<std::string, std::string> ConfigParser::getMimeTypes(void) const {
+
+}
+
+std::string ConfigParser::getErrorPagesDirectory(void) const {
+
+}
+
+bool ConfigParser::isDirectoryListeningEnabled(void) const {
+
+
+}
+
+bool ConfigParser::isCgiEnabled(void) const {
+
+}
