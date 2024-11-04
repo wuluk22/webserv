c26df5f622ff63189a8a5d4a9cd5b7b1e272523b
noureh10 <noureh.it@outlook.com>
2024-11-04 16:26:00 +0100

    Miscellaneous

--- src/configuration_file_parsing/ConfigParser.hpp
+++ src/configuration_file_parsing/ConfigParser.hpp
@@ -2,34 +2,27 @@
 #define CONSIGPARSER_HPP
 
 #include <iostream>
+#include <vector>
 #include <map>
 
-#define BAD_CONFIG_FILE "The configuration file you gave is not valid"
+#define BAD_CONFIG_FILE "The path of the configuration file you gave is not valid !"
+
+class Directive;
 
 class ConfigParser
 {
 	private:
-		std::string root_dir;
-		std::string log_file;
-		std::map<std::string, std::string> virtual_hosts;
-		std::map<std::string, std::string> mime_types;
-		bool enable_directory_listening;
-		std::string error_pages_directory;
-		bool enable_cgi;
+		const std::string path_of_configuration_file;
+		std::vector<Directive> all_directives;
+		ConfigParser(const std::string init_path);
 	public:
-		ConfigParser(std::string config_file_path);
-		~ConfigParser(void);
 		ConfigParser(const ConfigParser &copy);
-		ConfigParser& operator=(const ConfigParser &assign);
-
-		// Getter
-		std::string getRootDirectory(void) const;
-		std::string getLogFile(void) const;
-		std::map<std::string, std::string> getVirtualHosts(void) const;
-		std::map<std::string, std::string> getMimeTypes(void) const;
-		std::string getErrorPagesDirectory(void) const;
-		bool isDirectoryListeningEnabled(void) const;
-		bool isCgiEnabled(void) const;
+		ConfigParser& operator=(const ConfigParser &rhs);
+		~ConfigParser();
+		void createInstance(const std::string init_path);
+		ConfigParser getInstance(void);
+		const std::string& getPathOfConfigurationFile(void) const;
+		Directive getAllDirectives(void) const;
 };
 
 #endif
