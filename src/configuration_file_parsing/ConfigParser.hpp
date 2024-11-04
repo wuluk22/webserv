43447955604db37754f37b60077b9106ad008aa4
noureh10 <noureh.it@outlook.com>
2024-11-04 16:55:33 +0100

    Changing all directives vector to a map containing multiples directives from different servers

--- src/configuration_file_parsing/ConfigParser.hpp
+++ src/configuration_file_parsing/ConfigParser.hpp
@@ -2,27 +2,27 @@
 #define CONSIGPARSER_HPP
 
 #include <iostream>
-#include <vector>
 #include <map>
+#include <vector>
+#include <fstream>
+#include "Directive.hpp"
 
 #define BAD_CONFIG_FILE "The path of the configuration file you gave is not valid !"
-
-class Directive;
 
 class ConfigParser
 {
 	private:
 		const std::string path_of_configuration_file;
-		std::vector<Directive> all_directives;
+		std::map<unsigned int,std::vector<Directive>> servers_config;
 		ConfigParser(const std::string init_path);
 	public:
-		ConfigParser(const ConfigParser &copy);
-		ConfigParser& operator=(const ConfigParser &rhs);
+		ConfigParser(const ConfigParser &copy) = delete;
+		ConfigParser& operator=(const ConfigParser &rhs) = delete;
 		~ConfigParser();
-		void createInstance(const std::string init_path);
-		ConfigParser getInstance(void);
-		const std::string& getPathOfConfigurationFile(void) const;
-		Directive getAllDirectives(void) const;
+		ConfigParser& createInstance(const std::string init_path);
+		ConfigParser& getInstance(void);
+		std::string& getPathOfConfigurationFile(void) const;
+		std::map<unsigned int, std::vector<Directive>> getServersConfig(void) const;
 };
 
 #endif
