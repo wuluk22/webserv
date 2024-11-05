392894bdc0d1922f640c140a5b55c6f61c872b28
noureh10 <noureh.it@outlook.com>
2024-11-05 13:45:28 +0100

    Finishing the Singleton design, implemented a parametric exception and made some check before parsing

--- src/configuration_file_parsing/ConfigParser.cpp
+++ src/configuration_file_parsing/ConfigParser.cpp
@@ -5,22 +5,28 @@
 ConfigParser::ConfigParser(const std::string init_path) {
 	std::ifstream configuration_input_file;
 
-	if (!init_path.empty())
-		throw ConfigParser::ConfigException();
+	if (!init_path.empty() || !endsWith(init_path, ".conf"))
+		throw ConfigException(BAD_CONFIG_FILE);
 	configuration_input_file.open(init_path);
 	if (!configuration_input_file.is_open())
-		throw ConfigParser::ConfigException();
-	// TODO : Envoyer le fichier au casse pipe
+		throw ConfigException(NO_ACCESS);
+	
 }
 
-ConfigParser::~ConfigParser() {}
-
-ConfigParser& ConfigParser::createInstance(const std::string init_path) {
-
+bool ConfigParser::endsWith(const std::string path, const std::string extension) {
+	if (extension.size() > path.size())
+		return (false);
+	return (path.substr(path.size() - extension.size()) == extension);
 }
 
-ConfigParser& ConfigParser::getInstance(void) {
+ConfigParser::~ConfigParser() {
+	delete (this->instance);
+}
 
+ConfigParser& ConfigParser::getInstance(const std::string init_path = "") {
+	if (!instance)
+		instance = new ConfigParser(init_path);
+	return (*instance);
 }
 
 const std::string& ConfigParser::getPathOfConfigurationFile(void) const {
@@ -34,9 +40,13 @@ Directive ConfigParser::getServerConfig(unsigned int id) const {
 	if (wanted_server_config != _servers_config.end() && !wanted_server_config->second.empty())
 		return wanted_server_config->second.front();
 	else
-		throw ConfigParser::ConfigException();
+		throw ConfigException(NO_SERVER_CONFIG);
 }
 
+// EXCEPTION CLASS
+
+ConfigParser::ConfigException::ConfigException(const std::string& msg) : message(msg) {}
+
 const char* ConfigParser::ConfigException::what() const throw() {
-	// TODO : message d'erreur
+	return (message.c_str());
 }
--- src/configuration_file_parsing/ConfigParser.hpp
+++ src/configuration_file_parsing/ConfigParser.hpp
@@ -7,24 +7,30 @@
 #include <fstream>
 #include "Directive.hpp"
 
-#define BAD_CONFIG_FILE "The path of the configuration file you gave is not valid !"
+#define BAD_CONFIG_FILE "Invalid file"
+#define NO_ACCESS "Cannot acces the file"
+#define NO_SERVER_CONFIG "ID points to non-existant server configuration"
 
 class ConfigParser
 {
 	private:
+		ConfigParser* instance;
 		std::string _path_of_configuration_file;
 		std::map<unsigned int,std::vector<Directive>> _servers_config;
 		ConfigParser(const std::string init_path);
+		bool endsWith(const std::string path, const std::string extension);
 	public:
 		ConfigParser(const ConfigParser &copy) = delete;
 		ConfigParser& operator=(const ConfigParser &rhs) = delete;
 		~ConfigParser();
-		ConfigParser& createInstance(const std::string init_path);
-		ConfigParser& getInstance(void);
+		ConfigParser& getInstance(const std::string init_path);
 		const std::string& getPathOfConfigurationFile(void) const;
 		Directive getServerConfig(unsigned int id) const;
 		class ConfigException : public std::exception {
+			private:
+				std::string message;
 			public:
+				ConfigException(const std::string& msg);
 				virtual const char* what() const throw();
 		};
 };
