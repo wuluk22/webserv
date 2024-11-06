4a77380eade17a6ad0fd7aed6fc507c5b774ce5b
noureh10 <noureh.it@outlook.com>
2024-11-06 09:54:57 +0100

    Implementing the ServerConfig methods, need to work on Directive class. Might make it abstract

--- src/configuration_file_parsing/ServerConfig.cpp
+++ src/configuration_file_parsing/ServerConfig.cpp
@@ -1,41 +1,51 @@
 #include "ServerConfig.hpp"
-#include <vector>
 
 // Server Configuration class
 
-ServerConfig::ServerConfig(void) {}
+ServerConfig::ServerConfig(void) {
+    this->_server_params_defined = false;
+}
 
 ServerConfig::ServerConfig(const ServerConfig &copy) {
-
+    (*this) = copy;
 }
 
 ServerConfig::~ServerConfig() {}
 
 ServerConfig& ServerConfig::operator=(const ServerConfig& rhs) {
-
+    if (this != &rhs)
+    {
+        this->_server_params_defined = rhs._server_params_defined;
+        this->_all_directives.assign(rhs._all_directives.begin(), rhs._all_directives.end());
+    }
+    return (*this);
 }
 
 std::vector<Directive> ServerConfig::getAllDirectives(void) const {
-
+    return (this->_all_directives);
 }
 
 void ServerConfig::setDirective(Directive new_directive) {
-
+    this->_all_directives.push_back(new_directive);
 }
 
 void ServerConfig::addLocationDirective(s_common_params c_params, s_loc_params l_params) {
-
+    LocationBlock new_location_directive(c_params, l_params);
+    setDirective(new_location_directive);
 }
 
 void ServerConfig::addServerDirective(s_common_params c_params, s_server_params s_params) {
-    
+    if (this->_server_params_defined)
+        throw ConfigException(TWO_SERVER_BLOCK_DEFINITIONS);
+    ServerBlock new_server_directive(c_params, s_params);
+    setDirective(new_server_directive);
+
 }
 
 // Directive class and its derivatives
+// TODO : Change to abstract, no implementation needed
 
-Directive::Directive(void) {
-
-}
+Directive::Directive(void) {}
 
 Directive::Directive(const ServerConfig &copy) {
 
@@ -61,6 +71,8 @@ ServerBlock::~ServerBlock() {
 
 }
 
+// LocationBlock
+
 ServerBlock& ServerBlock::operator=(const ServerBlock &rhs) {
 
 }
@@ -78,3 +90,4 @@ LocationBlock::~LocationBlock() {}
 LocationBlock& LocationBlock::operator=(const LocationBlock &rhs) {
 
 }
+
