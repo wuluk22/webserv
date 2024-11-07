d0db44302cb31fabab0e27a61f85269ffb415d84
noureh10 <noureh.it@outlook.com>
2024-11-07 12:55:20 +0100

    Start of parser + token class implementation

--- src/configuration_file_parsing/ConfigParser.cpp
+++ src/configuration_file_parsing/ConfigParser.cpp
@@ -10,11 +10,12 @@ ConfigParser::ConfigParser(const std::string init_path) {
 	if (init_path.empty() || !endsWith(init_path, ".conf"))
 		throw BadPathException();
 	this->_path_of_configuration_file = init_path;
-	configuration_input_file.open(init_path);
+	configuration_input_file.open(init_path.c_str());
 	if (!configuration_input_file.is_open())
 		throw NoAccessException();
 	parseConfigurationFile(configuration_input_file);
 }
+
 ConfigParser::ConfigParser(const ConfigParser &copy) {
 	(void) copy;
 }
@@ -49,13 +50,99 @@ ServerConfig ConfigParser::getServerConfig(unsigned int id) const {
 		throw std::exception();
 }
 
+bool is_token_valid_multiple(const std::string& line, const std::string tokens[], size_t size) {
+	for (size_t i = 0; i < size; ++i) {
+		if (line.find(tokens[i]) != std::string::npos) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool is_token_valid(const std::string& line, const std::string token) {
+	return line == token;
+}
+
+bool is_only_whitespace(const std::string& str) {
+	for (size_t i = 0; i < str.size(); ++i) {
+		if (!std::isspace(str[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+#define C_SIZE 4
+#define S_SIZE 2
+#define L_SIZE 4
+#define B_PARAM 2
+
+/**
+ *  PARAMS THAT CAN ONLY BE DELCARED ONE TIME IN A RESPECTIVE BLOCK 
+ *  ROOT - CGI-PATH - CGI-EXTENSIONS - allowed_method
+ *  
+ */
 void ConfigParser::parseConfigurationFile(std::ifstream &configuration_file) {
-	(void) configuration_file;
+	std::string working_line;
+	std::string c_param[4] = {"root", "index", "auto_index", "client_max_body_size"};
+	std::string s_param[2] = {"server_name", "listen"};
+	std::string l_param[4] = {"cgi_path", "cgi_extensions", "alias", "allowed_methods"};
+	std::string b_param[2] = {"server", "location"};
+	std::string config_terminator = "end-server";
+	unsigned int tab_count = 0;
+	unsigned int location_deep = 0;
+	bool inside_server_block = false;
+	ADirective workingDirective;
+	TokenCounter Tk;
+
+	while (std::getline(configuration_file, working_line)) {
+		if (!inside_server_block && is_token_valid(working_line,b_param[0]) && tab_count == 0) {
+			std::cout << "inside" << std::endl;
+			inside_server_block = true;
+			tab_count++;
+		}
+		else if (inside_server_block && is_token_valid(working_line, config_terminator)) {
+			std::cout << "outside" << std::endl;
+			inside_server_block = false;
+		}else if(!inside_server_block && is_only_whitespace(working_line)){
+			std::cout << "skip" << "\n";
+			tab_count = 0;
+		} else {
+			std::cout << "problem" << "\n";
+			
+		}
+	}
+}
+
+// Token counter class
+
+TokenCounter::TokenCounter() {
+	blockStack.push(std::map<std::string, int>());
+}
+
+void TokenCounter::enterBlock() {
+	blockStack.push(std::map<std::string, int>());
+}
+
+void TokenCounter::exitBlock() {
+	if (blockStack.size() > 1) {
+		blockStack.pop();
+	} else {
+		std::cerr << "Error: No block to exit!" << std::endl;
+	}
+}
+
+void TokenCounter::incrementToken(const std::string& token) {
+	blockStack.top()[token]++;
+}
+
+int TokenCounter::getTokenCount(const std::string& token) {
+	return blockStack.top()[token];
 }
 
 int main(void) {
 	try {
-		ConfigParser *config = ConfigParser::getInstance("test.con");
+		ConfigParser *config = ConfigParser::getInstance("test.conf");
 	} catch (BadPathException &e)
 	{
 		std::cout << e.what() <<std::endl;
