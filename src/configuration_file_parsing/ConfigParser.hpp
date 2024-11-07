f3faec9a12c363dc05177258b19e790e84f0fff3
noureh10 <noureh.it@outlook.com>
2024-11-07 23:48:14 +0100

    Changing Token class position in the ConfigParser.hpp

--- src/configuration_file_parsing/ConfigParser.hpp
+++ src/configuration_file_parsing/ConfigParser.hpp
@@ -10,26 +10,9 @@
 #include "ConfigException.hpp"
 
 #define CONFIG_TERMINATOR "end-server"
+#define LOC_TERMINATOR "end-location"
 #define B_SERVER "server"
 #define B_LOC "location"
-
-class ConfigParser
-{
-	private:
-		static ConfigParser* instance;
-		std::string _path_of_configuration_file;
-		std::map<unsigned int,std::vector<ServerConfig> > _servers_config;
-		ConfigParser(const std::string init_path);
-		bool endsWith(const std::string path, const std::string extension);
-		void parseConfigurationFile(std::ifstream &configuration_file);
-		void processBlock(std::ifstream &configuration_file, std::string working_line);
-	public:
-		~ConfigParser();
-		ConfigParser(const ConfigParser &copy);
-		static ConfigParser* getInstance(const std::string init_path);
-		const std::string& getPathOfConfigurationFile(void) const;
-		ServerConfig getServerConfig(unsigned int id) const;
-};
 
 class TokenCounter {
 	public:
@@ -40,6 +23,24 @@ class TokenCounter {
 		int getTokenCount(const std::string& token);
 	private:
 		std::stack<std::map<std::string, int> > blockStack;
+};
+
+class ConfigParser
+{
+	private:
+		static ConfigParser* instance;
+		std::string _path_of_configuration_file;
+		std::map<unsigned int,std::vector<ServerConfig> > _servers_config;
+		ConfigParser(const std::string init_path);
+		bool endsWith(const std::string path, const std::string extension);
+		void parseConfigurationFile(std::ifstream &configuration_file);
+		void processBlock(std::ifstream &config_file, std::string w_line, TokenCounter &Tk, size_t &level);
+	public:
+		~ConfigParser();
+		ConfigParser(const ConfigParser &copy);
+		static ConfigParser* getInstance(const std::string init_path);
+		const std::string& getPathOfConfigurationFile(void) const;
+		ServerConfig getServerConfig(unsigned int id) const;
 };
 
 #endif
