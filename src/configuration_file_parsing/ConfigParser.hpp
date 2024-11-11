ba6de5ddfa8df1f49ee2654d9f68d147a774a0aa
noureh10 <noureh.it@outlook.com>
2024-11-11 01:35:13 +0100

    Adding utils functions to the header, changing the disposition of other procedures

--- src/configuration_file_parsing/ConfigParser.hpp
+++ src/configuration_file_parsing/ConfigParser.hpp
@@ -9,20 +9,21 @@
 #include "ServerConfig.hpp"
 #include "ConfigException.hpp"
 
-#define CONFIG_TERMINATOR "end-server"
+#define SERVER_TERMINATOR "end-server"
 #define LOC_TERMINATOR "end-location"
 #define B_SERVER "server"
 #define B_LOC "location"
 
 class TokenCounter {
-	public:
-		TokenCounter();
-		void enterBlock();
-		void exitBlock();
-		void incrementToken(const std::string& token);
-		int getTokenCount(const std::string& token);
-	private:
-		std::stack<std::map<std::string, int> > blockStack;
+public:
+	TokenCounter();
+	void enterBlock();
+	void exitBlock();
+	void incrementToken(const std::string& token);
+	int getTokenCount(const std::string& token) const;
+
+private:
+	std::stack<std::map<std::string, int> > blockStack;
 };
 
 class ConfigParser
@@ -34,10 +35,19 @@ class ConfigParser
 		static ConfigParser* instance;
 		std::string _path_of_configuration_file;
 		std::map<unsigned int,std::vector<ServerConfig> > _servers_config;
+		
+		std::string trim(const std::string& str);
+		std::vector<std::string> split(const std::string& str, char delimiter);
+		bool is_only_whitespace(const std::string& str);
+		bool is_token_valid_multiple(const std::string& line, const std::vector <std::string> tokens);
+		bool is_token_valid(const std::string& line, const std::string token);
+
 		ConfigParser(const std::string init_path);
 		bool endsWith(const std::string path, const std::string extension);
 		void parseConfigurationFile(std::ifstream &configuration_file);
-		void processBlock(std::ifstream &config_file, std::string w_line, TokenCounter &Tk, size_t &level);
+		void processLocationBlock(std::ifstream &config_file, std::string w_line, TokenCounter &Tk,  size_t &current_line, LocationBlock *loc_directive);
+		void processServerBlock(std::ifstream &config_file, std::string w_line,  size_t &current_line);
+		void isPair(std::vector <std::string> splitted_arguments);
 	public:
 		~ConfigParser();
 		ConfigParser(const ConfigParser &copy);
