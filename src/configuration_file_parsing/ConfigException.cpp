db369814247fc178d428ba98ff96f71e11bac274
noureh10 <noureh.it@outlook.com>
2024-11-11 01:37:21 +0100

    Macro change

--- src/configuration_file_parsing/ConfigException.cpp
+++ src/configuration_file_parsing/ConfigException.cpp
@@ -1,17 +1,5 @@
 #include "ConfigException.hpp"
 
 const char * ConfigException::what() {
-	return ("Default Config Exception");
-}
-
-const char * BadPathException::what() {
-	return (BAD_FILE);
-}
-
-const char * NoAccessException::what() {
-	return (NO_ACCESS);
-}
-
-const char * NoServerConfigException::what() {
-	return (NO_SERVER_CONFIG);
+	return (DEFAULT_CONFIG_EXCEPTION);
 }
