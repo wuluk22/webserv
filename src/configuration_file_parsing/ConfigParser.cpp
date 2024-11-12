0ce572f14513f0a769f33b8b67c4164cb344d0b5
noureh10 <noureh.it@outlook.com>
2024-11-12 02:50:42 +0100

    Refactoring, adding a case for location directive with no URI

--- src/configuration_file_parsing/ConfigParser.cpp
+++ src/configuration_file_parsing/ConfigParser.cpp
@@ -7,31 +7,30 @@ ConfigParser* ConfigParser::instance = NULL;
 ConfigParser::ConfigParser(const std::string init_path) {
 	std::ifstream configuration_input_file;
 
-	// LOCATION TOKENS
+	std::string l_items[] = { "cgi_path", "cgi_extensions", "alias", "allowed_method", "return" };
+    initializeVector(l_params, l_items, ARRAY_SIZE(l_items));
+    std::string s_items[] = { "server_name", "listen" };
+    initializeVector(s_params, s_items, ARRAY_SIZE(s_items));
+    std::string c_items[] = { "root", "index", "auto_index", "client_max_body_size" };
+    initializeVector(c_params, c_items, ARRAY_SIZE(c_items));
+    std::string non_repeat_s[] = { "root" };
+    initializeVector(non_repeat_s_token, non_repeat_s,ARRAY_SIZE(non_repeat_s));
+    std::string non_repeat_l[] = { "root", "cgi_extensions", "cgi_path" };
+    initializeVector(non_repeat_l_token, non_repeat_l, ARRAY_SIZE(non_repeat_l));
 
-	l_params.push_back("cgi_path");
-	l_params.push_back("cgi_extensions");
-	l_params.push_back("alias");
-	l_params.push_back("allowed_method");
-	l_params.push_back("return");
-	
-	// SERVER TOKENS
-	s_params.push_back("server_name");
-	s_params.push_back("listen");
-
-	// COMMON TOKENS
-	c_params.push_back("root");
-	c_params.push_back("index");
-	c_params.push_back("auto_index");
-	c_params.push_back("client_max_body_size");
-
-	if (init_path.empty() || !endsWith(init_path, ".conf"))
+	if (init_path.empty() || !endsWith(init_path, FILE_EXT))
 		throw ConfigException();
 	this->_path_of_configuration_file = init_path;
 	configuration_input_file.open(init_path.c_str());
 	if (!configuration_input_file.is_open())
 		throw ConfigException();
 	parseConfigurationFile(configuration_input_file);
+}
+
+void ConfigParser::initializeVector(std::vector<std::string>& vec, std::string items[], size_t count) {
+    for (size_t i = 0; i < count; ++i) {
+        vec.push_back(items[i]);
+    }
 }
 
 ConfigParser::ConfigParser(const ConfigParser &copy) {
@@ -80,17 +79,18 @@ void ConfigParser::processLocationBlock(std::ifstream &config_file, std::string 
 		last_position = config_file.tellg();
 		if (is_only_whitespace(working_line))
 			continue;
-		
 		working_line_splitted = split(working_line, ' ');
-		if (is_token_valid(working_line_splitted[0], B_LOC) && working_line_splitted.size() == 2) {
+		if (is_token_valid(working_line_splitted[0], B_LOC) && working_line_splitted.size() == 2)
 			processLocationBlock(config_file, working_line, token_counter, current_line);
+		else if (is_token_valid(working_line_splitted[0], B_LOC) && working_line_splitted.size() != 2) {
+			std::cerr << ERROR_HEADER << NO_URI_LOCATION << AL << current_line << RESET << std::endl;
+			throw ConfigException();
 		} else if (is_token_valid(working_line_splitted[0], LOC_TERMINATOR) && working_line_splitted.size()) {
 			break;
-		} else if (is_token_valid_multiple(working_line_splitted[0], l_params) 
-			|| is_token_valid_multiple(working_line_splitted[0], c_params)) {
+		} else if (is_token_valid_multiple(working_line_splitted[0], l_params) || is_token_valid_multiple(working_line_splitted[0], c_params)) {
 			token_counter.incrementToken(working_line_splitted[0]);
-			if (token_counter.getTokenCount("cgi_path") > 1 || token_counter.getTokenCount("cgi_extensions") > 1 || token_counter.getTokenCount("root") > 1 || token_counter.getTokenCount("return") > 1) {
-				std::cerr << ERROR_HEADER << TOKEN_REPEATED << AL << current_line << std::endl;
+			if (!token_counter.oneOccurenceCheck(non_repeat_l_token)) {
+				std::cerr << ERROR_HEADER << TOKEN_REPEATED << AL << current_line << RESET << std::endl;
 				throw ConfigException();
 			}
 		}
@@ -110,22 +110,19 @@ void ConfigParser::processServerBlock(std::ifstream &config_file, std::string wo
 		last_position = config_file.tellg();
 		if (is_only_whitespace(working_line))
 			continue;
-
 		working_line_splitted = split(working_line, ' ');
-		if (is_token_valid(working_line, B_LOC) && working_line_splitted.size() == 2) {
+		if (is_token_valid(working_line_splitted[0], B_LOC) && working_line_splitted.size() == 2) {
 			processLocationBlock(config_file, working_line, token_counter, current_line);
 		} else if (is_token_valid(working_line_splitted[0], SERVER_TERMINATOR) && working_line_splitted.size()) {
 			break;
 		} else if (is_token_valid_multiple(working_line_splitted[0], c_params) || is_token_valid_multiple(working_line_splitted[0], s_params)) {
 			token_counter.incrementToken(working_line_splitted[0]);
-			if (token_counter.getTokenCount("root") > 1) {
+			if (!token_counter.oneOccurenceCheck(non_repeat_s_token)) {
 				std::cerr << ERROR_HEADER << TOKEN_REPEATED << AL << current_line << std::endl;
 				throw ConfigException();
 			}
-		} else if (is_token_valid(working_line_splitted[0], B_LOC) && working_line_splitted.size() == 2) {
-			processLocationBlock(config_file, working_line, token_counter, current_line);
-		} else {
-			std::cerr << ERROR_HEADER << INVALID_TOKEN << AL << current_line << std::endl;
+		}else {
+			std::cerr << ERROR_HEADER << TOKEN_REPEATED << AL << current_line << RESET << std::endl;
 			throw ConfigException();
 		}
 	}
@@ -147,7 +144,7 @@ void ConfigParser::parseConfigurationFile(std::ifstream &config_file) {
 		if (working_line_splitted[0] == B_SERVER && working_line.size()) {
 			processServerBlock(config_file, working_line, current_line);
 		} else {
-			std::cerr << ERROR_HEADER << TOKEN_POSITION_MISMATCH << AL << std::endl;
+			std::cerr << ERROR_HEADER << TOKEN_POSITION_MISMATCH << AL << current_line << RESET <<std::endl;
 			throw ConfigException();
 		}
 	}
