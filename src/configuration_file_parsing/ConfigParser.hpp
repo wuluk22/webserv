54c3005433c0a8a179a309b4613a2d199bebc496
noureh10 <noureh.it@outlook.com>
2024-11-13 01:30:06 +0100

    Changing signature of method used for parsing

--- src/configuration_file_parsing/ConfigParser.hpp
+++ src/configuration_file_parsing/ConfigParser.hpp
@@ -59,8 +59,8 @@ class ConfigParser {
 		void parseConfigurationFile(std::ifstream &configuration_file);
 		void processLocationBlock(std::ifstream &config_file, std::string w_line, TokenCounter &Tk,  size_t &current_line, LocationBlock *loc_directive);
 		void processServerBlock(std::ifstream &config_file, std::string w_line,  size_t &current_line);
-		void processDirectiveLoc(LocationBlock directive, std::vector <std::string> args);
-		void processDirectiveServ(ServerBlock directive, std::vector <std::string> args);
+		void processDirectiveLoc(LocationBlock directive, std::string working_line, std::vector<std::string> splitted_working_line);
+		void processDirectiveServ(ServerBlock directive, std::string working_line, std::vector<std::string> splitted_working_line);
 	public:
 		~ConfigParser();
 		ConfigParser(const ConfigParser &copy);
