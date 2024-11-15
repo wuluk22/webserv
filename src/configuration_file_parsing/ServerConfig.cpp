a40ad9fec3896151b371884ee8953bde8211fd97
noureh10 <noureh.it@outlook.com>
2024-11-15 02:00:05 +0100

    Implementing setters method, not tested yet. Need thorough check

--- src/configuration_file_parsing/ServerConfig.cpp
+++ src/configuration_file_parsing/ServerConfig.cpp
@@ -61,10 +61,23 @@ bool ADirective::setRoot(std::string root_args) {
 }
 
 bool ADirective::setIndex(std::vector<std::string>index_args) {
+	std::set <std::string> unique_index;
 	if (index_args.empty())
 		return (false);
-	// IMPLEMENT THE REST TOMORROW, TIRED
+	unique_index.insert(index_args.begin(), index_args.end());
+	for (std::set<std::string>::iterator it = unique_index.begin(); it != unique_index.end(); ++it)
+    	_common_params._index.push_back(*it);
+	return (true);
 }
+
+void ADirective::setAutoIndex(bool value) {
+	_common_params._auto_index = value;
+}
+
+void ADirective::setClientMaxBodySize(unsigned int body_size_value) {
+	_common_params._client_max_body_size = body_size_value;
+}
+
 // ServerBlock
 
 ServerBlock::ServerBlock(void) {}
@@ -80,6 +93,31 @@ ServerBlock::ServerBlock(const ServerBlock &copy) {
 
 ServerBlock::~ServerBlock() {}
 
+bool ServerBlock::isValidServerName(std::string name) {
+	bool last_dot_occurence;
+
+	last_dot_occurence = false;
+	if (name.empty())
+		return (false);
+	if (!std::isalpha(name[0]))
+		return (false);
+	for (int i = 0; i < name.size(); i++) {
+		char c = name[i];
+		if (std::isalnum(c) || c == '-' || c == '.') {
+			if ((i == 0 || i == name.size() - 1) && (c == '.' || c == '-')) 
+				return false;
+			if (c == '.') {
+				if (last_dot_occurence) 
+					return (false);
+				last_dot_occurence = true;
+			} else
+				last_dot_occurence = false;
+		} else
+			return (false);
+	}
+	return (true);
+}
+
 s_server_params ServerBlock::getServerParams(void) const {
 	return (this->_server_params);
 }
@@ -91,6 +129,27 @@ ServerBlock& ServerBlock::operator=(const ServerBlock &rhs) {
 		this->_server_params = rhs._server_params;
 	}
 	return (*this);
+}
+
+bool ServerBlock::setServerName(std::vector<std::string> server_names) {
+	if (server_names.empty())
+		return (false);
+	for (int i = 0; i < server_names.size(); i++) {
+		if (isValidServerName(server_names[i])) {
+			_server_params._server_name.push_back(server_names[i]);
+		} else
+			return (false);
+	}
+	return (true);
+}
+
+bool ServerBlock::setListeningPort(std::vector<unsigned int> listening_ports) {
+	std::set <unsigned int> unique_ports;
+	if (listening_ports.empty())
+		return (false);
+	for (std::set<unsigned int>::iterator it = unique_ports.begin(); it != unique_ports.end(); ++it)
+    	_server_params._listen.push_back(*it);
+	return (true);
 }
 
 // LocationBlock
@@ -119,4 +178,59 @@ LocationBlock& LocationBlock::operator=(const LocationBlock &rhs) {
 
 s_loc_params LocationBlock::getLocationParams(void) const {
 	return (this->_location_params);
+}
+
+bool LocationBlock::setCgiPath(std::string path_args) {
+	if (path_args.empty())
+		return (false);
+	_validator.setPath(path_args);
+	if (_validator.exists() && _validator.isFile() && _validator.isExecutable()) {
+		_location_params._cgi_path = path_args;
+		return (true);
+	}
+	return (false);
+}
+
+bool LocationBlock::setUri(std::string uri_args) {
+	if (uri_args.empty())
+		return (false);
+	_validator.setPath(uri_args);
+	if (_validator.exists() && _validator.isDirectory())
+	{
+		_location_params._uri = uri_args;
+		return (true);
+	}
+	return (false);
+}
+
+bool LocationBlock::setAlias(std::string alias_path) {
+		if (alias_path.empty())
+		return (false);
+	_validator.setPath(alias_path);
+	if (_validator.exists() && _validator.isDirectory())
+	{
+		_location_params._alias = alias_path;
+		return (true);
+	}
+	return (false);
+}
+
+bool LocationBlock::setAllowedMethods(unsigned char allowed_method) {
+	if (allowed_method == 0) {
+		return (false);
+	}
+	_location_params._allowed_methods = allowed_method;
+	return (true);
+}
+
+bool LocationBlock::isGetAllowed(void) {
+	return (_location_params._allowed_methods & GET) != 0;
+}
+
+bool LocationBlock::isPostAllowed(void) {
+	return (_location_params._allowed_methods & POST) != 0;
+}
+
+bool LocationBlock::isDeleteAllowed(void) {
+	return (_location_params._allowed_methods & DELETE) != 0;
 }
