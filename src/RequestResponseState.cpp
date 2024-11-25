f64855378728cc622e1998ea12477812208b468f
Camerrone Legros <clegros@fu-r10-p4.s19.be>
2024-11-25 15:30:19 +0100

    alles goed met het recv() hein

--- src/RequestResponseState.cpp
+++ src/RequestResponseState.cpp
@@ -1,13 +1,11 @@
 #include "RequestResponseState.hpp"
 
-RRState::RRState() : _requestBuffer(""), _requestComplete(false), _responseBuffer(""), _bytesSent(0) {}
+RRState::RRState() {}
 
 RRState::~RRState() {}
 
 HttpRequestHandler&     RRState::getRequest() { return _request; }
 HttpResponseHandler&	RRState::getResponse() { return _response; }
-int&                    RRState::getResultRecv() { return _resultRecv; }
 
 void                RRState::setRequest(HttpRequestHandler& req) { this->_request = req; }
 void                RRState::setResponse(HttpResponseHandler& res) { this->_response = res; }
-void                RRState::setResultRecv(int& resultRecv) {this->_resultRecv = resultRecv; }
