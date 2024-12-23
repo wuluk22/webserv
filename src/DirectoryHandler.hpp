870d6763de9f5a11267e9135863ee559eff09007
noureh10 <noureh.it@outlook.com>
2024-12-23 17:10:55 +0100

    Updating directory handler

--- src/DirectoryHandler.hpp
+++ src/DirectoryHandler.hpp
@@ -9,13 +9,15 @@
 # include <ctime>
 # include <sstream>
 # include <iostream>
+# include "configuration_file_parsing/utils/PathValidator.hpp"
+# include "Logger.hpp"
 
 struct FileInfo
 {
     std::string name;
     std::string size;
-    std::string modified;
-    bool isDirectory;
+    std::string     modified;
+    bool            isDirectory;
     
     FileInfo(const std::string& n, const std::string& s, const std::string& m, bool d);
 };
@@ -23,20 +25,19 @@ struct FileInfo
 class DirectoryHandler
 {
     private:
-        static std::string				humanReadableSize(size_t bytes);
-        static std::string				formatTime(time_t time);
-        static bool						compareFileInfo(const FileInfo& a, const FileInfo& b);
-        static std::string				generateBreadcrumbs(const std::string& path);
-        static std::string				getMimeType(const std::string& path);
-
+        std::string				humanReadableSize(size_t bytes);
+        std::string				formatTime(time_t time);
+        static bool				compareFileInfo(const FileInfo& a, const FileInfo& b);
+        std::string				generateBreadcrumbs(const std::string& path);
+        std::string				getMimeType(const std::string& path);
+        PathValidator            _validator;
     public:
         DirectoryHandler();
         ~DirectoryHandler();
-
-        static std::vector<FileInfo>	getDirectoryListing(const std::string& dir_path);
-        static std::string				generateDirectoryPage(const std::string& path, const std::vector<FileInfo>& files);
-        static bool						createDirectory(const std::string& path);
-        static bool						isDirectory(const std::string& path);
+        std::vector<FileInfo>	getDirectoryListing(const std::string& dir_path);
+        std::string				generateDirectoryPage(const std::string& path, const std::vector<FileInfo>& files);
+        bool						createDirectory(const std::string& path);
+        bool						isDirectory(const std::string& path);
 };
 
 #endif
