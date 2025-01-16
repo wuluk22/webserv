929d0619acbf326efd1b311a9349a41f233697c3
noureh10 <noureh.it@outlook.com>
2025-01-16 03:41:33 +0100

    Adding exception message  for cgi-subroute

--- src/configuration_file_parsing/config_parser/ConfigException.hpp
+++ src/configuration_file_parsing/config_parser/ConfigException.hpp
@@ -41,6 +41,7 @@
 #define DEPENDS_ON_NO_MATCH ERROR_HEADER "No match found for depends on directive," AB RESET
 #define ALIAS_NO_MATCH ERROR_HEADER "No match found for alias directive," AB RESET
 #define BAD_CGI_PATH ERROR_HEADER "No corresponding path for the CGI," AB RESET
+#define CGI_PATH_HAS_SUBROUTE ERROR_HEADER "CGI Path cannot have a subroute," AB RESET
 
 // SERVER HEADER ERROR MESSAGES
 #define NOT_VALID_SERVER_NAME ERROR_HEADER "Invalid server name," AB RESET
